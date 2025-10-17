c9e1323791015258677b7ccf0396573b67f1d0ee
richmontato <danielwibowo66@gmail.com>
2025-10-17 17:36:47 +0700

    feat(auth): implement email verif (views, routes, controllers)
    Menambahkan rebase
    
    :wq
    Menambahkan rebase
    
    :wq
    Menambahkan rebase
    
    :wq
    Menambahkan rebase
    
    :wq

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -14,7 +16,7 @@ class RegisterController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'confirmed', Password::min(8)->mixedCase()->numbers()],
         ]);
 
         $user = User::create([
@@ -23,8 +25,10 @@ class RegisterController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        event(new Registered($user));
+
         Auth::login($user);
 
-        return redirect()->route('dashboard');
+        return redirect()->route('verification.notice');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,60 @@
 
 use App\Http\Controllers\AlkitabController;
 use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Route;
 
+//Landing Page
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('guest');
 
+//Login Page
 Route::get('login', function () {
     return view('auth.login');
 })->name('login');
 
-Route::post('login', [LoginController::class, '_invoke'])->name('login.attempt');
+Route::post('login', [LoginController::class, '_invoke'])
+->name('login.attempt')
+->middleware('throttle:5,1');
 
+//Register Page
 Route::get('register', function (){
     return view('auth.registration');
 })->name('register');
 
-Route::post('register', [App\Http\Controllers\RegisterController::class, '__invoke'])->name('register.attempt');
+Route::post('register', [App\Http\Controllers\RegisterController::class, '__invoke'])
+->name('register.attempt')
+->middleware('throttle:5,1');
 
-Route::view('dashboard', 'dashboard')->name('dashboard');
+//Logout
+Route::post('logout', function () {
+    Auth::guard('web')->logout();
+
+    Session::invalidate();
+    Session::regenerateToken();
+
+    return redirect('/');
+})->name('logout');
+//Dashboard
+Route::view('dashboard', 'dashboard')->name('dashboard')->middleware(['auth', 'verified']);
+
+// Email verification
+Route::get('/email/verify', function () {
+    return view('auth.verification-notice');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (Illuminate\Foundation\Auth\EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Illuminate\Http\Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('status', 'verification-link-sent');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 
 Route::get('alkitab', [AlkitabController::class, 'index'])->name('alkitab');
 
